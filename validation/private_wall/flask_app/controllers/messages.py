b3376593e03b89725f2d003a24bb2685108d93e5
brandontaylor156 <brandon.david4@outlook.com>
2022-07-17 17:07:35 -0700

    modularized

---
+++ validation/private_wall/flask_app/controllers/messages.py
@@ -0,0 +1,23 @@
+from flask_app import app, render_template, request, redirect, session, flash
+from flask_app.models import message as message_module
+
+@app.route("/user/message/<int:id>", methods=['POST'])
+def insert_message(id):
+    if not message_module.Message.validate_message(request.form):
+        return redirect("/user/dashboard")
+
+    data = {
+        'message': request.form['message'],
+        'sender_id': session['user_id'],
+        'receiver_id': id
+    }
+    new_message = message_module.Message.insert_message(data)
+    return redirect("/user/dashboard")
+
+@app.route("/message/delete/<int:id>")
+def message_delete(id):
+    data = {
+        'id': id
+    }
+    message_module.Message.delete_message(data)
+    return redirect("/user/dashboard")
--- validation/private_wall/flask_app/controllers/users.py
+++ validation/private_wall/flask_app/controllers/users.py
@@ -1,4 +1,3 @@
-from crypt import methods
 from flask_app import app, render_template, request, redirect, session, flash
 from flask_app.models import user, message as message_module
 from flask_bcrypt import Bcrypt
@@ -30,7 +29,6 @@ def insert_user():
 
 @app.route("/user/dashboard")
 def user_dashboard():
-
     data = {
         'id' : session['user_id']
     }
@@ -71,27 +69,6 @@ def login_user():
     session['user_id'] = user_in_db.id
     session['first_name'] = user_in_db.first_name
 
-    return redirect("/user/dashboard")
-
-@app.route("/user/message/<int:id>", methods=['POST'])
-def insert_message(id):
-    if not message_module.Message.validate_message(request.form):
-        return redirect("/user/dashboard")
-
-    data = {
-        'message': request.form['message'],
-        'sender_id': session['user_id'],
-        'receiver_id': id
-    }
-    new_message = message_module.Message.insert_message(data)
-    return redirect("/user/dashboard")
-
-@app.route("/message/delete/<int:id>")
-def message_delete(id):
-    data = {
-        'id': id
-    }
-    message_module.Message.delete_message(data)
     return redirect("/user/dashboard")
 
 @app.route("/user/logout")
